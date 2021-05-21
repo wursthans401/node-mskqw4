f7ca6d0b3855a7fb5ff4eda3ebe2e142c0486640
wursthans401 <wursthans401@yandex.com>
2021-05-21 13:44:29 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 console.log(`Hello Node.js v${process.versions.node}!`);
 console.log("Hans Wurst der I.");
 console.log("Freitag");
+var i;
+for (i = 1; i <=15; i++) {
+  console.log("Zahl:", i);
+}
